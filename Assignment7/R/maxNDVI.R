4184d39b9cb6f2213b06eaaaad41ab18a562aade
Koen Veenenbos <koenrv@hotmail.com>
2017-01-17 21:04:56 +0100

    Delete maxNDVI.R

--- Assignment7/R/maxNDVI.R
+++
@@ -1,9 +0,0 @@
-#Koen Veenenbos & Tim jak team JakVeenenbos
-#January 2017
-
-MaxNDVI <- function(month, full_month){
-  x <- which.max(month$fullmonth)
-  return(value(x))
-}
-MaxNDVI(Jan, January)
-which.max(Jan$ January)
