17c60c7c7241196d3029c79406e0ef38ed259907
Koen Veenenbos <koenrv@hotmail.com>
2017-01-17 21:04:45 +0100

    Delete NDVImonth.R

--- Assignment7/R/NDVImonth.R
+++
@@ -1,6 +0,0 @@
-#Koen Veenenbos & Tim jak team JakVeenenbos
-#January 2017
-
-CalcNDVI <- function(LSData, month, MunData){
-  extract(LSData[[month]]*0.0001, MunData, fun = mean, na.rm=TRUE, sp = TRUE)
-}
