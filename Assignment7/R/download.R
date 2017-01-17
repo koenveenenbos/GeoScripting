a2053e6ead0e9ba3e0164c9d1cf2d45f697d5d06
Koen Veenenbos <koenrv@hotmail.com>
2017-01-17 21:04:52 +0100

    Delete download.R

--- Assignment7/R/download.R
+++
@@ -1,7 +0,0 @@
-#Koen Veenenbos & Tim jak team JakVeenenbos
-#January 2017
-
-down_unzip <- function(link, path){
-  download.file(url = link, destfile = path, method = "auto")
-  unzip(path, exdir = "Data")
-}
